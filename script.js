1ab40d85b6acb709be1928f0c241713477e8e01c
henry <hjbishop@live.co.uk>
2022-09-16 16:45:40 +0900

    fix small errors and clean up code

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ newGame.addEventListener('click', () => {
     resetScore();
   });
 
-let choice = document.querySelector("#selections");
+let choice = document.querySelector(".selections");
 choice.addEventListener("click", event => {
     if (event.target.nodeName == "BUTTON") {
-      let playerSelecion = (event.target.textContent);
+      let playerSelecion = (event.target.textContent.toLowerCase());
       let computerSelection = getCompChoice();
-      document.getElementById("player").innerHTML = playerSelecion;
-      document.getElementById("computer").innerHTML = computerSelection;
+      document.getElementById("player").innerHTML = playerSelecion.toUpperCase();
+      document.getElementById("computer").innerHTML = computerSelection.toUpperCase();
       playRound(playerSelecion, computerSelection); 
       }
     });
@@ -19,32 +19,44 @@ choice.addEventListener("click", event => {
 function playRound(playerSelecion, computerSelection) {
     
     if (playerSelecion === computerSelection) {
-        document.getElementById("currentRound").innerHTML = ("tie");
+        document.getElementById("currentRound").innerHTML = ("Tie!");
     } else if (playerSelecion === "rock" && computerSelection === "scissors") {
         userScore++;
-        document.getElementById("currentRound").innerHTML = ("you win rock beats scissors");
+        document.getElementById("currentRound").innerHTML = ("You win! Rock beats scissors");
     } else if (playerSelecion === "paper" && computerSelection === "rock") {
         userScore++
-        document.getElementById("currentRound").innerHTML = ("you win paper beats rock")
+        document.getElementById("currentRound").innerHTML = ("You win! Paper beats rock")
     } else if (playerSelecion === "scissors" && computerSelection === "paper") {
         userScore++
-        document.getElementById("currentRound").innerHTML = ("you win scissors beats paper")
+        document.getElementById("currentRound").innerHTML = ("You win! Scissors beats paper")
     } else if (playerSelecion === "paper" && computerSelection === "scissors") {
         computerScore++
-        document.getElementById("currentRound").innerHTML = ("you lose scissors beats paper")
+        document.getElementById("currentRound").innerHTML = ("You lose! Scissors beats paper")
     } else if (playerSelecion === "rock" && computerSelection === "paper") {
         computerScore++ 
-        document.getElementById("currentRound").innerHTML = ("you lose paper beats rock")
+        document.getElementById("currentRound").innerHTML = ("You lose! Paper beats rock")
     } else if (playerSelecion === "scissors" && computerSelection === "rock") {
         computerScore++
-        document.getElementById("currentRound").innerHTML = ("you lose rock beats scissors")
+        document.getElementById("currentRound").innerHTML = ("You lose! Rock beats scissors")
     } if (userScore >= 5) {
         document.getElementById("result").innerHTML = "You win";
+        //document.querySelectorAll("#selections").disabled = true;
+        disable();
+        
     } else if (computerScore >= 5) {
         document.getElementById("result").innerHTML = "Computer wins";
+        //document.querySelectorAll("#selections").disabled = true;
+        disable();
         }
+        
     document.getElementById("userScore").innerHTML = userScore;
     document.getElementById("compScore").innerHTML = computerScore;
+}
+
+function disable() {
+   document.querySelectorAll('.selections button').forEach(elem => {
+    elem.disabled = true;
+  });
 }
 
 function resetScore() {
@@ -52,11 +64,15 @@ function resetScore() {
     computerScore = 0;
     playerSelecion = "";
     computerSelection = "";
+    document.querySelectorAll('.selections button').forEach(elem => {
+        elem.disabled = false;
+      });
     document.querySelector("#userScore").innerHTML = userScore;
     document.querySelector("#compScore").innerHTML = computerScore;
     document.getElementById("player").innerHTML = playerSelecion;
     document.getElementById("computer").innerHTML = computerSelection;
     document.getElementById("currentRound").innerHTML = "";
+    document.getElementById("result").innerHTML = "";
  }
 
 function getCompChoice() {
